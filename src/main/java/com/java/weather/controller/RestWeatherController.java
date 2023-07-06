72794aa4ad0a79661b12cd7dd5cb2dbe12490951
kbledzki <krzysztofbledzki@gmail.com>
2023-07-06 11:30:26 +0200

    fix rest controler

--- src/main/java/com/java/weather/controller/RestWeatherController.java
+++ src/main/java/com/java/weather/controller/RestWeatherController.java
@@ -3,10 +3,10 @@ package com.java.weather.controller;
 import com.java.weather.model.Weather;
 import com.java.weather.service.WeatherService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class RestWeatherController {
     private final WeatherService weatherService;
 
@@ -15,7 +15,7 @@ public class RestWeatherController {
     }
 
     @GetMapping("api/v1/weather/{city}")
-    public Weather getWeather(String city) {
+    public Weather getWeather(@PathVariable String city) {
         return weatherService.getWeather(city);
     }
 }
